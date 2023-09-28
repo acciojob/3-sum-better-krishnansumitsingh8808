b679879a43583e3b9c32f51feba8f18cb9bf3f44
Sumit Raj Singh <133523998+krishnansumitsingh8808@users.noreply.github.com>
2023-09-28 07:47:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function threeSum(arr, target) {
-// write your code here
+let sum=NEGATIVE_INFINITY;
+	for(let i in arr){
+		sum+=arr[i];
+	}
+	if(sum==target||sum==target-1||sum==target+1||sum==target+2||sum==target-2){
+		return sum;
+	}
   
 }
 
